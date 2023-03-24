4736553c10159757457beaa9fc33541c260c763e
Gh0St_Labs <kordan.goat@gmail.com>
2023-03-24 11:41:53 +0530

    Sending the Mail

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+from send_the_mail import email
+import random as rd
+
 # Articles to Hand Over
 # Title
 # Description
@@ -6,13 +9,45 @@ import requests
 # Author
 # Content
 
+news_topics = 'tesla'
+
 api_key = "418b0abdbc8740fb80b9a96abb362e3f"
 url = "https://newsapi.org/v2/everything?" \
-      "q=tesla&from=2023-02-23&sortBy=publishedAt&api" \
-      "Key=418b0abdbc8740fb80b9a96abb362e3f"
+      f"q={news_topics}&from=2023-02-23&sortBy=publishedAt&api" \
+      "Key=418b0abdbc8740fb80b9a96abb362e3f&language=en"
 
+random_subjects = ["Today's News!", "Breaking News!", "News Has Arrived!", "Daily News Has Arrived",
+                   "Today's News Highlights!", "Shattering News Highlights!", "Today's Highlights!"]
+choose_subjects = rd.choice(random_subjects)
 
 read_the_requests = requests.get(url=url)
-content = read_the_requests.json()
+contents = read_the_requests.json()
 
-print(content['articles'])
+title = ""
+author = ""
+description = ""
+url = ""
+news_content = ""
+body = ""
+
+for one_artice in contents['articles'][:20]:
+      title = one_artice['title']
+      author = one_artice['author']
+      description = one_artice['description']
+      url = one_artice['url']
+      news_content = one_artice['content']
+      str1 = "News:"
+      str2 = "Author:"
+      str3 = "Description:"
+      str4 = "URL:"
+      if title and author and description and url and news_content is not None:
+            body = f"Subject: {choose_subjects}" + "\n" + body + "\n" + str1 + " " + title + \
+                   "\n" + str2 + " " + author + "\n" + str3 + " " + description + "\n" + str4 + \
+                   " " + url + "\n" + "\n"
+
+
+
+body = body.encode("utf-8")
+email(body)
+
+
---
+++ send_the_mail.py
@@ -0,0 +1,17 @@
+import ssl
+import smtplib
+import os
+
+def email(the_message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    gmail = "syondukeabraham@gmail.com"
+    password = os.getenv("PWD")
+
+    reciever = "syondukeabraham@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(gmail, password)
+        server.sendmail(gmail, reciever, msg=the_message)
